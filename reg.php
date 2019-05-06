e196b4b0daf2c0a0b2a29b2c500438da59c82cfd
ChihTai <49051958+ChihTai@users.noreply.github.com>
2019-05-06 11:24:35 +0800

    login-檢查註冊表單欄位資料
    login-檢查註冊表單欄位資料

--- reg.php
+++ reg.php
@@ -1,24 +1,89 @@
 <?php
+  include_once "base.php";
 
   if(!empty($_POST)){
     $acc=$_POST['acc'];
     $pw=$_POST['pw'];
     $name=$_POST['name'];
 
+  //檢查資料空白
+        
+  if($acc=="" || $pw=="" || $name==""){
+
+    echo "請輸入資料，不可空白";
+    $chk1=false;
+
+  }else{
+
+    $chk1=true;
+  }
   
-  //建立新增資料的語法
-  $sql="insert into user (`acc`,`pw`,`name`) values('$acc','$pw','$name')";
+  //檢查資料長度
+  if(strlen($acc) < 4 || strlen($acc) > 12 ){
+
+    echo "帳號請在4-12碼之間";
+    $chk2=false;
+
+  }else{
+   
+    $chk2=true;
   
-  //送出新增語法
-  $res=$pdo->query($sql);
-    if($res){
-      echo "新增成功";
-    }else{
-      echo "新增異常";
+  }
+
+  //檢查全為數字
+  $chkNum=0;
+  for($i=0;$i<strlen($acc);$i++){
+
+    $str=substr($acc,$i,1);
+    if(ord($str) >= 48 && ord($str) <= 57){
+      $chkNum++;
     }
   }
 
+//檢查全為英文
+  $chkEng=0;
+  for($i=0;$i<strlen($acc);$i++){
+
+    $str=substr($acc,$i,1);
+    if((ord($str) >= 65 && ord($str) <= 90) || (ord($str) >= 97 && ord($str) <= 122) ){
+      $chkEng++;
+    }
+  }
+
+  //檢查特殊符號
+  $chkSym=0;
+  for($i=0;$i<strlen($acc);$i++){
+
+    $str=substr($acc,$i,1);
+    if(!(ord($str) >= 65 && ord($str) <= 90) && !(ord($str) >= 97 && ord($str) <= 122) && !(ord($str) >= 48 && ord($str) <= 57) ){
+      $chkSym++;
+    }
+
+  }
+
+  if($chkSym > 0 || $chkEng==strlen($acc) || $chkNum==strlen($acc) ){
+    echo "有特殊符號，而且全英文或數字";
+    $chk3=false;
+  }else{
+    $chk3=true;
+  }
+
+
+  if($chk1==true && $chk2==true && $chk3==true){
+     //建立新增資料的語法
+     $sql="insert into user (`acc`,`pw`,`name`) values('$acc','$pw','$name')";
+    
+     //送出新增語法
+     $res=$pdo->query($sql);
+       if($res){
+         echo "新增成功";
+       }else{
+         echo "新增異常";
+       }
+    }
+}
 ?>
+
 
   <form action="index.php?do=reg" method="post">
   <table>
@@ -40,3 +105,4 @@
     </tr>
   </table>
 </form>
+
