bcc1d1f7c6d96bff5c8e4074930df054689fbcfc
ChihTai <49051958+ChihTai@users.noreply.github.com>
2019-05-06 14:41:25 +0800

    add nav.php
    add nav.php

--- nav.php
+++ nav.php
@@ -1,14 +1,21 @@
 <div id="nav">
       <ul class="nav">
+        <li><a href="index.php">首頁</a></li>
         <li><a href="index.php?do=login">登入</a></li>
         <li><a href="index.php?do=reg">註冊會員</a></li>
         <li>徵才訊息</li>
-        <?php
-          if(!empty($_SESSION['login'])){
-            
-            echo "<li> <a href='logout.php'>登出</a></li>";
-            
-          }
-        ?>
+
+        <?php 
+        //利用session來判斷使用者的登入狀態用以決定是不是要顯示登出的功能
+        if(!empty($_SESSION['login'])){
+          echo "<li><a href='logout.php'>登出</a></li>";
+      }
+      ?>
+      <?php 
+        //利用session來判斷使用者的登入狀態用以決定是不是要顯示登出的功能
+        if(!empty($_SESSION['login']) && $_SESSION['login']=='admin'){
+          echo "<li><a href='index.php?do=admin'>管理頁面</a></li>";
+      }
+      ?>
       </ul>
     </div>
